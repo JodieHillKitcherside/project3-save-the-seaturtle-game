1078f646553dbfc2c02385880e3fcd015d117ec8
JodieHillKitcherside <>
2023-08-28 16:38:47 +0000

    Add emojis and each emoji function to emojis.py

--- emojis.py
+++ emojis.py
@@ -12,7 +12,7 @@ class emoji:
     self.lightblueheart = " \U+1FA75 \n"
     self.whiteheart = " \U+1F90D \n"
     # Gestures 
-    self.wave = " \U+1F44B \n"
+    self.handwave = " \U+1F44B \n"
     self.okay = " \U+1F44C \n"
     self.clapping = " \U+1F44F \n"
     # Marine life 
@@ -27,6 +27,7 @@ class emoji:
     self.reef = " \U+1FAB8 \n"
     self.jellyfish = " \U+1FABC	 \n"
     self.lobster = " \U+1F99E \n"
+    self.seal = " \U+1F9AD \n"
     self.squid = " \U+1F991 \n"
     self.crab = " \U+1F980 \n"
     # Ocean Transport 
@@ -36,10 +37,10 @@ class emoji:
     self.divingmask = " \U+1F93F \n"
     self.compass = " \U+1F9ED \n"
     # Keys
-    self.up = " \U+2B06 \n"
-    self.down = " \U+2B07 \n"
-    self.right = " \U+27A1 \n"
-    self.left = " \U+2B05 \n"
+    self.e_up = " \U+2B06 \n"
+    self.e_down = " \U+2B07 \n"
+    self.e_right = " \U+27A1 \n"
+    self.e_left = " \U+2B05 \n"
     # Warnings
     self.biohazard = " \U+2623 \n"
     # Mexico flag
@@ -50,4 +51,100 @@ class emoji:
     self.pirateflag = " \U+1F3F4 U+200D U+2620 U+FE0F \n"
     self.skull = " \U+2620 \n"
 
+
+"""
+Defines all functions for each emoji
+"""
+def lightblueheart(self):
+    return f"{self.lightblueheart}"
+
+def whiteheart(self):
+    return f"{self.whiteheart}"
+
+def handwave(self):
+    return f"{self.handwave}"
+
+def okay(self):
+    return f"{self.okay}"
+
+def clapping(self):
+    return f"{self.clapping}"
+
+def seaturtle(self):
+    return f"{self.seaturtle}"
+
+def wave(self):
+    return f"{self.wave}"
+
+def dolphin(self):
+    return f"{self.dolphin}"
+
+def tropicalfish(self):
+    return f"{self.tropicalfish}"
+
+def blowfish(self):
+    return f"{self.blowfish}"
+
+def shark(self):
+    return f"{self.shark}"
+
+def octopus(self):
+    return f"{self.octopus}"
+
+def shell(self):
+    return f"{self.shell}"
+
+def reef(self):
+    return f"{self.reef}"
+
+def jellyfish(self):
+    return f"{self.jellyfish}"
+
+def lobster(self):
+    return f"{self.lobster}"
+
+def seal(self):
+    return f"{self.seal}"
+
+def squid(self):
+    return f"{self.squid}"
+
+def crab(self):
+    return f"{self.crab}"
+
+def buoy(self):
+    return f"{self.buoy}"
+
+def motorboat(self):
+    return f"{self.motorboat}"
+
+def compass(self):
+    return f"{self.compass}"
+
+def divingmask(self):
+    return f"{self.divingmask}"
+
+def e_up(self):
+    return f"{self.e_up}"
+
+def e_down(self):
+    return f"{self.e_down}"
+
+def e_right(self):
+    return f"{self.e_right}"
+
+def e_left(self):
+    return f"{self.e_left}"
+
+def biohazard(self):
+    return f"{self.biohazard}"
+
+def mexico(self):
+    return f"{self.mexico}"
+
+def pirateflag(self):
+    return f"{self.pirateflag}"
+
+def skull(self):
+    return f"{self.skull}"
 
