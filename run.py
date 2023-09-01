e9cdaad36bfdec8366a9c8928bc49827ea47a0a1
JodieHillKitcherside <>
2023-09-01 15:54:44 +0000

    Delete general.py due to import error and ciricular issue

--- run.py
+++ run.py
@@ -1,8 +1,3 @@
-from general import (
-    slowprint,
-    escape,
-    check_user_input
-)
 import sys
 from statements import (
     beachPartyExitPrint,
@@ -19,6 +14,82 @@ import random
 from pygame.locals import *
 import pyfiglet
 from emojis import Emoji
+
+import sys
+import time
+import pygame
+from pygame.locals import *
+# from run import main
+
+
+pygame.init()
+
+
+def slowprint(all_strings):
+    # Styling all strings
+    for each_character in all_strings + "\n":
+        sys.stdout.write(each_character)
+        sys.stdout.flush()
+        time.sleep(1/17)
+
+
+def escape():
+    # Implementing a hotkey
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return main()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                main()
+
+
+def check_user_input(input_str):
+    # Validates all user input
+    # Prints prompt if blank
+    for event in pygame.event.get():
+        if event.type == "":
+            print("Please enter your input")
+        else:
+            None
+
+import sys
+import time
+
+import pygame
+from pygame.locals import *
+# from run import main
+
+
+pygame.init()
+
+
+def slowprint(all_strings):
+    # Styling all strings
+    for each_character in all_strings + "\n":
+        sys.stdout.write(each_character)
+        sys.stdout.flush()
+        time.sleep(1/17)
+
+
+def escape():
+    # Implementing a hotkey
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return main()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                main()
+
+
+def check_user_input(input_str):
+    # Validates all user input
+    # Prints prompt if blank
+    for event in pygame.event.get():
+        if event.type == "":
+            print("Please enter your input")
+        else:
+            None
+
 
 
 pygame.init()
